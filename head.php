04e5536f5d00d0a8711887281d1b735b7cbe39ba
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2020-01-23 19:47:50 +0530

    head.php is added with picture code.png

---
+++ head.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style>
+  body{
+    background-color:white;
+    color: black;
+  }
+.navv{
+  background-color:white;
+
+}
+.navv a{
+  color: black;
+}
+.navv a:hover{
+  color:gray;
+}
+.he{
+  font-size: 45px;
+  font-weight: bold;
+  margin-top: 50px;
+  color: gray;
+}
+.im{
+  box-shadow: 30px 30px 30px gray;
+}
+.al{
+  color:black;
+  font-weight: bold;
+  font-size: 15px;
+}
+.al1{
+  color:black;
+}
+</style>
+  </head>
+<body>
+
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navv">
+      <a class="navbar-brand" href="#">Algo Repo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <a class="nav-link" href="#">Alogorithms<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Comptitive Programming </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Database</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#">Operating System</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Fundamentals</a>
+            </li>
+
+        </ul>
+      </div>
+    </nav>
+
+
+<br><br><br>
+
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-4">
+    <h1 class="he">We Make  <span style="color:black;font-size:55px;font-weight:bolder;">Technology</span>  Repository For Your Future</h1>
+  </div>
+  <div class="col-sm-1"></div>
+
+  <div class="col-sm-5">
+     <img class="img-responsive im" src="code.png" alt="Chania" width="100%" height="100%"> 
+  </div>
+   
+</div>
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-5">
+  <span class="al">Alogorithms | Compitative Programming | Database | Operating System </span>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-10">
+  <span class="al1">Algo Repo is a platform where we try to explain in simple terms the logic behind data-strutures, algorithms ,operating system ,database & also have blog posts on various web-development technologies.</span>
+</div>
+</div>
+
+
+
+
+  
+   
+ 
+
+    <!-- <h2>Basic form</h2>
+    <form action="/" method="POST">
+      <div class="form-group ">
+        <label for="email">User Name:</label>
+        <input type="text" class="form-control" id="text" placeholder="Enter username" name="username">
+      </div>
+      <div class="form-group ">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      </div>
+      <div class="form-group">
+        <label for="Phone">Phone:</label>
+        <input type="phone" class="form-control" id="phone" placeholder="Enter phone" name="phone">
+      </div>
+   -->
+
+</div>
+
+
+</body>
+</html>
